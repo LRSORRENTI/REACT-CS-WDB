a4b1dea3c83ee9a26eb97d53a6d9407039d0e1b5
Luke <lrsorrenti@hotmail.com>
2023-10-09 10:31:54 -0500

    Added logic conditional check, both inline and outside of reurn statement

--- 02-Props/Props/src/components/DoubleDice.tsx
+++ 02-Props/Props/src/components/DoubleDice.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-export default function DoubleDice(): JSX.Element {
+export default function DoubleDice(){
     const num1: number = Math.floor(Math.random() * 3) + 1;
     const num2: number = Math.floor(Math.random() * 3) + 1;
     let truthyFalsy: string; 
-    if (num1 !== num2) {
-        truthyFalsy = "false";
-    } else {
-        truthyFalsy = "true";
-    }
+    // if (num1 !== num2) {
+    //     truthyFalsy = "false";
+    // } else {
+    //     truthyFalsy = "true";
+    // }
+    if(num1 !== num2 ? truthyFalsy = "false" : truthyFalsy = 'true')
     return (
         <div>
             <h1>Num1: {num1}</h1>
             <h1>Num2: {num2}</h1>
             <h2>Was a match? {truthyFalsy}</h2>
+            <h2>Also can use inline JS conditional 
+                check: {num1 !== num2 ? truthyFalsy = "false" : truthyFalsy = 'true'}
+            </h2>
         </div>
     );
 }
