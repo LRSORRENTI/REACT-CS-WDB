b876a51d9a283a969be496a1ad4489f8937ece37
Luke <lrsorrenti@hotmail.com>
2023-10-09 09:22:57 -0500

    added more to die roll props example

--- 02-Props/Props/src/components/Die.jsx
+++ 02-Props/Props/src/components/Die.jsx
@@ -1,5 +1,10 @@
 export default function Die({numSides}){
     console.log(numSides)
     const roll = Math.floor(Math.random() * numSides) + 1;
-    return<p> Die Roll: {roll}</p>
+    return (
+        <div>
+        <h2>Number of sides: {numSides}</h2>
+    <p>Die Roll: {roll}</p>
+    </div>
+    )
 }
