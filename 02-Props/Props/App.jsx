79d67961676fc8993e44d527da3e2bf3f2dcda31
Luke <lrsorrenti@hotmail.com>
2023-10-10 09:52:17 -0500

    Added map method to render array of items as li's

--- 02-Props/Props/App.jsx
+++ 02-Props/Props/App.jsx
@@ -5,12 +5,14 @@ import Die from './src/components/Die'
 import DayPicker from './src/components/DayOfWeekPicker.'
 import DoubleDice from './src/components/DoubleDice'
 import Heading from './src/components/Heading'
+import ColorList from './src/components/ColorList'
 function App() {
   // Below we omit the prop value for the 
   // first greeter, defaults to 'Luke', but for 
   // the second greeter call we omit the day= prop
   // which has no default value therefore nothing 
   // is rendered it's just 'How are you on this' 
+   /* <ColorList colors={["aqua", "pink", "lime"]} /> */
  return (
   <div>
  {/* <h1>Hello world!</h1>
@@ -23,6 +25,8 @@ function App() {
  <Heading text={"I'm a heading"}/>
  <DayPicker values={['Mon ', 'Tues ', 'Weds ', 'Thurs ', 'Fri ']} />
  <DoubleDice />
+ <ColorList colors={["aqua", "pink", "lime"]}/>
+
  </div>
   )
 }
