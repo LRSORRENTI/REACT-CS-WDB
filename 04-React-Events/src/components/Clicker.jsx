791a536b14663601bbd0f1d02f88fd367df40029
Luke <lrsorrenti@hotmail.com>
2023-10-16 08:53:58 -0500

    Moved notes into docs folder, also added mouseOver event example

--- 04-React-Events/src/components/Clicker.jsx
+++ 04-React-Events/src/components/Clicker.jsx
@@ -2,11 +2,14 @@ function handleClick(){
     console.log("clicked!!!")
 }
 
+function mousedOver(){
+    console.log('Moused over!!')
+}
 
 export default function Clicker(){
     return(
         <div>
-            <p>Click The Button</p>
+            <p onMouseOver={mousedOver}>Click The Button</p>
             <button onClick={handleClick}>Click</button>
         </div>
     )
