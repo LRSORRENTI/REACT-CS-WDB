6917c61f84468e04490c03fe9a89bcf93eda3ef2
Luke <lrsorrenti@hotmail.com>
2023-10-22 09:46:00 -0500

    Added two functions that use map to change every element to hearts or dollars

--- 06-State-In-Depth/state-in-depth/src/App.jsx
+++ 06-State-In-Depth/state-in-depth/src/App.jsx
@@ -3,13 +3,14 @@ import EmojiClicker from './components/EmojiClicker'
 // import ScoreKeeper from './components/ScoreKeeper'
 // import Counter from './components/Counter'
 // import InitializerFunc from './components/InitializerFunc'
-
+import MakeAllItmesHearts from './components/MakeAllItemsHearts'
 function App() {
 
   return (
     <>
     <h1>State in depth</h1>
-   <EmojiClicker/>
+   {/* <EmojiClicker/> */}
+    <MakeAllItmesHearts/>
     </>
   )
 }
