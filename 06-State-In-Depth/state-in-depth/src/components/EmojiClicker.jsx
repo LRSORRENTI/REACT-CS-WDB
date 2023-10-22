be5b89dc741f1d3f78babf58e59043825d9ca952
Luke <lrsorrenti@hotmail.com>
2023-10-22 08:32:58 -0500

    Added uuid to each new enoji added to the array, setting up the deletion process for any emoji in the array, not just the first or last

--- 06-State-In-Depth/state-in-depth/src/components/EmojiClicker.jsx
+++ 06-State-In-Depth/state-in-depth/src/components/EmojiClicker.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
+import {v4 as uuid } from 'uuid';
 
 export default function EmojiClicker(){
     // For this example we'll use an array as state,
     // then loop over that state to render each emoji
-    const [emojis, setEmojis] = useState([":)"]);
+    const [emojis, setEmojis] = useState([{id: uuid(), emoji: ':D'}]);
 
     function addEmoji(){
         // We need a copy of emojis to change the 
@@ -16,27 +17,26 @@ export default function EmojiClicker(){
 
             // We make a copy of the old array with 
             // ...oldEmojis
-            return [...oldEmojis, ":("]
+            return [...oldEmojis, {id: uuid(), emoji: ':P'}]
         })
     }
 
-    const removeEmoji = (idx) => {
-        setEmojis((oldEmojis => {
-            const newArray = [...oldEmojis];
-            newArray.splice(idx, 1);
-            return newArray;
-        }))
-    }
+    // const removeEmoji = (idx) => {
+    //     setEmojis((oldEmojis => {
+    //         const newArray = [...oldEmojis];
+    //         newArray.splice(idx, 1);
+    //         return newArray;
+    //     }))
+    // }
 
     return (
         <div>
             {emojis.map(emoji => {
                 return (
-                    <span style={{fontSize: "4rem"}}>{emoji}</span>
+                    <span key={emoji.id} style={{fontSize: "4rem"}}>{emoji.emoji}</span>
                 )
             })}
             <button onClick={addEmoji}>Add emoji</button>
-            <button onClick={removeEmoji}>Remove 1</button>
         </div>
     )
 }
