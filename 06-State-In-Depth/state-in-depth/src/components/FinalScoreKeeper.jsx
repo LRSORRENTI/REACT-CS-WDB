87b84b734ebd3a3fd370635cdee83b6d121c229e
Luke <lrsorrenti@hotmail.com>
2023-10-23 09:01:04 -0500

    Added increment score functionality for the onclick

--- 06-State-In-Depth/state-in-depth/src/components/FinalScoreKeeper.jsx
+++ 06-State-In-Depth/state-in-depth/src/components/FinalScoreKeeper.jsx
@@ -3,14 +3,25 @@ import { useState } from "react"
 
 export default function FinalScoreKeeper({numPlayers=3, target=5}){
     
-    const [scores, setScores] = userState(new Array(numPlayers).fill(0));
+    const [scores, setScores] = useState(new Array(numPlayers).fill(0));
   
+    const incrementScore = (idx) => {
+        setScores(prevScores => {
+         return prevScores.map((score, i) => {
+            if(idx === i) return score + 1;
+            return score;
+         });
+        });
+    };
+
     return(
     <div>
        <h1>Score Keeper</h1>
        <ul>
         {scores.map((score, idx) => {
-            return <li key={idx}>Player{idx + 1}</li>
+            return <li key={idx}>Player{idx + 1}: {score}
+            <button onClick={() => incrementScore(idx)}>+1</button>
+            </li>
         })}
        </ul>
     </div>
