ae2c49bb29ab12223d4bad17d3216171fe637a13
Luke <lrsorrenti@hotmail.com>
2023-10-24 08:32:10 -0500

    added single die componennt

--- 07-Component-Design/component-design/src/App.jsx
+++ 07-Component-Design/component-design/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Die from '../components/Die/Die'
 
 function App() {
 
   return (
     <>
-    <h1>State in depth</h1>
+    <h1>Component Design</h1>
+    <Die val={7}/>
     </>
   )
 }
