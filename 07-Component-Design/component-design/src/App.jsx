655d57f4844050c1f58da1ef53247f3ab615e63e
Luke <lrsorrenti@hotmail.com>
2023-10-24 08:18:56 -0500

    added notes on how to structure components, usually components are either presentational- UI or Logical or responsible to handling the state and logic of things

--- 07-Component-Design/component-design/src/App.jsx
+++ 07-Component-Design/component-design/src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css'
-import EmojiClicker from './components/EmojiClicker'
-import FinalScoreKeeper from './components/FinalScoreKeeper'
-// import ScoreKeeper from './components/ScoreKeeper'
-// import Counter from './components/Counter'
-// import InitializerFunc from './components/InitializerFunc'
-import MakeAllItmesHearts from './components/MakeAllItemsHearts'
+
 function App() {
 
   return (
     <>
     <h1>State in depth</h1>
-   {/* <EmojiClicker/> */}
-    {/* <MakeAllItmesHearts/> */}
-    <FinalScoreKeeper numPlayers={12} target={15}/>
     </>
   )
 }
