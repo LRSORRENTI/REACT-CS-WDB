a4e4df79237e83363aee735431fbe9dbfcd23820
Luke <lrsorrenti@hotmail.com>
2023-10-26 08:31:56 -0500

    practicing passing state update functions

---
+++ 07-Component-Design/component-design/components/BoxGrid/BoxGrid.jsx
@@ -0,0 +1,18 @@
+import './BoxGrid.css'
+import Box from '../Box/Box'
+
+export default function BoxGrid() {
+    return (
+    <div className='BoxGrid'>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/>
+        <Box/> 
+    </div>
+    )
+}
