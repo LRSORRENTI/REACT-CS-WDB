9a03ee3d6fc02f71cb700aac963c537c5c1f428a
Luke <lrsorrenti@hotmail.com>
2023-11-03 09:21:46 -0500

    Added useEffect example using async function API data fetch

--- 09-Effects/React-Effects/src/App.jsx
+++ 09-Effects/React-Effects/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './components/Counter'
+import QuoteFetcher from './components/QuoteFetcher'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <Counter/>
+      <QuoteFetcher/>
   
     </>
   )
