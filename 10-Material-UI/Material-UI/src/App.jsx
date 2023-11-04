aad03e172efe763a9080dd912d293fdf06b439ed
Luke <lrsorrenti@hotmail.com>
2023-11-04 08:51:07 -0500

    added intro to MUI, added a few components

---
+++ 10-Material-UI/Material-UI/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import IconButton from '@mui/material/IconButton';
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+    <>
+   <Button size="large" onClick={() => alert("hi")} variant="contained">Contained</Button>
+   <Button size="medium" color="error" onClick={() => alert("hi")} variant="contained">Contained</Button>
+   <Button size="small" color="success" onClick={() => alert("hi")} variant="contained">Contained</Button>
+   <Stack direction="row" spacing={2}>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+    </Stack>
+    
+    </>
+  )
+}
+
+export default App
